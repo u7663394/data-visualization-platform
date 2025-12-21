29b8bdd5cc5993311b1ca3a46d2e563df585371b
u7663394 <u7663394@anu.edu.au>
2025-12-21 22:38:59 +0800

    Extract Toast notification function

--- js/common.js
+++ js/common.js
@@ -1,2 +1,14 @@
 // 配置axios基地址(baseURL)
 axios.defaults.baseURL = "https://hmajax.itheima.net";
+
+// 抽取提示函数
+function showToast(msg) {
+  // 1. 实例化
+  const toastDom = document.querySelector(".my-toast");
+  const toast = new bootstrap.Toast(toastDom);
+  // 2. 修改内容
+  toastDom.querySelector(".toast-body").innerText = msg;
+  // 3. 显示
+  toast.show();
+}
+showToast("Hello World!");
