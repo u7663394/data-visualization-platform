dec50171f5a288ed0e65a3997c038f1690d84a62
u7663394 <u7663394@anu.edu.au>
2025-12-21 23:43:15 +0800

    User login feature completed

---
+++ js/login.js
@@ -0,0 +1,41 @@
+/* 
+  用户登录功能：
+    1. 收集并校验数据
+    2. 提交
+    3. 缓存响应数据
+    4. 跳转到首页
+*/
+document.querySelector("#btn-login").addEventListener("click", async () => {
+  // 1. 收集数据
+  const form = document.querySelector(".login-form");
+  const data = serialize(form, { hash: true, empty: true });
+  // 2. 校验 (非空 + 长度)
+  const { username, password } = data;
+  if (!username || !password) {
+    showToast("用户名和密码不能为空！");
+    return;
+  }
+  if (
+    username.length < 8 ||
+    username.length > 30 ||
+    password.length < 6 ||
+    password.length > 30
+  ) {
+    showToast("用户名长度为8-30，密码长度为6-30");
+    return;
+  }
+  // 3. 提交
+  try {
+    const res = await axios.post("/login", { username, password });
+    showToast(res.data.message);
+    // 4. 缓存响应数据
+    localStorage.setItem("token", res.data.data.token);
+    localStorage.setItem("username", res.data.data.username);
+    // 5. 跳转到首页
+    setTimeout(() => {
+      window.location.href = "./index.html";
+    }, 1450);
+  } catch (error) {
+    showToast(error.response.data.message);
+  }
+});
