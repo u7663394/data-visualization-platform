344e0a5c19f6e60ebbd8c77047362942b14283d4
u7663394 <u7663394@anu.edu.au>
2025-12-22 13:49:33 +0800

    Statistics feature on index completed

--- js/index.js
+++ js/index.js
@@ -4,3 +4,25 @@ checkLogin();
 renderUsername();
 // 退出登录功能
 logout();
+
+/* 
+  首页数据统计功能: 
+    1. 调用接口
+    2. 渲染数据
+*/
+async function getData() {
+  // 1. 调用接口
+  const token = localStorage.getItem("token");
+  const res = await axios({
+    url: "/dashboard",
+    headers: {
+      Authorization: token,
+    },
+  });
+  // 2. 渲染数据
+  const overview = res.data.data.overview;
+  Object.keys(overview).forEach((key) => {
+    document.querySelector(`.${key}`).innerText = overview[key];
+  });
+}
+getData();
