4dd1be3c8a602a1a9f3f550ade59afac942ea931
u7663394 <u7663394@anu.edu.au>
2025-12-22 14:10:22 +0800

    Handle expired token on index page

--- js/index.js
+++ js/index.js
@@ -9,20 +9,38 @@ logout();
   首页数据统计功能: 
     1. 调用接口
     2. 渲染数据
+  首页登录token过期/被篡改:
+    1. 判断token失效(401)
+    2. 删除缓存并提示用户
+    3. 返回登录页面 
 */
 async function getData() {
-  // 1. 调用接口
-  const token = localStorage.getItem("token");
-  const res = await axios({
-    url: "/dashboard",
-    headers: {
-      Authorization: token,
-    },
-  });
-  // 2. 渲染数据
-  const overview = res.data.data.overview;
-  Object.keys(overview).forEach((key) => {
-    document.querySelector(`.${key}`).innerText = overview[key];
-  });
+  try {
+    // 1. 调用接口
+    const token = localStorage.getItem("token");
+    const res = await axios({
+      url: "/dashboard",
+      headers: {
+        Authorization: token,
+      },
+    });
+    // 2. 渲染数据
+    const overview = res.data.data.overview;
+    Object.keys(overview).forEach((key) => {
+      document.querySelector(`.${key}`).innerText = overview[key];
+    });
+  } catch (error) {
+    // 1. 判断token失效(401)
+    if (error.response.status === 401) {
+      // 2. 删除缓存并提示用户
+      localStorage.removeItem("token");
+      localStorage.removeItem("username");
+      showToast("账号已过期，请重新登录或注册！");
+      // 3. 返回登录页面
+      setTimeout(() => {
+        window.location.href = "./login.html";
+      }, 1500);
+    }
+  }
 }
 getData();
